0f62ccc78dc0301e87d29c418ee2543e6e81fbe5
Kazutaka Homma <special18fender@gmail.com>
2023-03-30 18:12:44 +0900

    Initial Commit

---
+++ swiftuitest/swiftuitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuitestApp.swift
+//  swiftuitest
+//
+//  Created by Kazutaka Homma on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct swiftuitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
