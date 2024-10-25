6df0fc56a7d1b7761a4fe514342c6072381b5d87
Kazutaka Homma <kaz@flexfitnessapp.com>
2024-10-25 10:11:41 +0900

    Fix memory leak issue

--- swiftuitest/ContentView.swift
+++ swiftuitest/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
         .padding()
+        .onAppear(perform: {
+            let temp = VM()
+            Task {
+                await temp.myActor.start()
+            }
+        })
     }
 }
 
---
+++ swiftuitest/MyActor.swift
@@ -0,0 +1,65 @@
+//
+//  MyActor.swift
+//  swiftuitest
+//
+//  Created by Kazutaka Homma on 2024-10-25.
+//
+
+import Combine
+import Foundation
+
+class VM {
+    public let myActor = MyActor()
+
+    deinit {
+        print("deinit \(self)")
+        myActor.end()
+    }
+}
+
+actor MyActor {
+    deinit {
+        print("deinit \(self)")
+    }
+
+    let subject = PassthroughSubject<Int, Never>()
+    var publisher: AnyPublisher<Int, Never> {
+        subject.eraseToAnyPublisher()
+    }
+
+    var count = 0
+
+    func start() {
+        Task {
+            for await i in publisher.asyncStream() {
+                handle(i)
+            }
+            print("leaving asyncStream Task")
+        }
+
+        for i in 0 ..< 1 {
+            subject.send(i)
+        }
+    }
+
+    func handle(_ i: Int) { count += i }
+
+    nonisolated func end() {
+        Task { await subject.send(completion: .finished) }
+    }
+}
+
+public extension AnyPublisher where Failure == Never {
+    func asyncStream() -> AsyncStream<Output> {
+        AsyncStream { continuation in
+            let cancellable = sink { _ in
+                continuation.finish()
+            } receiveValue: { value in
+                continuation.yield(value)
+            }
+            continuation.onTermination = { _ in
+                cancellable.cancel()
+            }
+        }
+    }
+}
